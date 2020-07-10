7acbb2b6509ac08f0c3ffd39fe9904e4ad117b25
zdylg <zdylg@1314.com>
2020-07-10 10:47:31 +0800

    day10 1&2

---
+++ work/day10(router_vant)/router-demo/src/routes/index.js
@@ -0,0 +1,41 @@
+import home from "../pages/home"
+import about from "../pages/about"
+import news from "../pages/news"
+import message from "../pages/message"
+import user from "../pages/user"
+import userDetail from "../pages/userDetail"
+export default [
+  {
+    path:"/home",
+    component:home,
+    children:[
+      {path:"news",component:news},
+      {path:"message",component:message},
+      {path:"",redirect:"news"}
+    ]
+  },
+  {path:"/about",component:about},
+  {
+    path:"/user",
+    component:user,
+    children:[
+      // props:true vue-router默认只会传params
+      {
+        path:":id",
+        component:userDetail,
+        props(route){
+          return {
+            id:route.params.id,
+            name:route.query.name
+          }
+        }},
+      {path:"",redirect:"2"}
+    ]
+  },
+  {path:"/",redirect:"/about"}
+]
+
+
+// {path:"/user/:id",component:user},
+// {path:"/user",redirect:"/user/1"},
+
