040a88e25e55cf23485774364be7b03c7b6f2526
zdylg <zdylg@1314.com>
2020-07-11 15:51:23 +0800

    day11 -5

--- work/day11(vant_axios_vuex)/axios-demo/src/pages/contact.vue
+++ work/day11(vant_axios_vuex)/axios-demo/src/pages/contact.vue
@@ -56,6 +56,8 @@
       },
       //决定了当前要展示的联系人
       currentContact() {
+       /* 新增一个联系人 必须是先更新list 再更新id
+        删除一个联系人 必须先更新id 再更新list*/
         const id = this.chosenContactId;
         return id !== null ? this.list.filter((item) => item.id === id)[0] : {};
       },
@@ -89,17 +91,24 @@
         let body ="";
         if (this.isEdit) {
           //修改联系人的逻辑
-          this.list = this.list.map((item) =>
-            item.id === info.id ? info : item
-          );
+          body = await this.$axios.put("/contact/edit",{name,tel,id	},{
+            baseURL:"http://localhost:9000/api"
+          })
+          await this.updateList()
+
         } else {
           //新增联系人的逻辑  往数据库插入了一条数据 可是界面上的list并没有得到更新
-          body = await this.$axios.post("/contact/new/json",{name, tel},{
+          let data = new FormData();
+          data.append("name",name)
+          data.append("tel",tel)
+          body = await this.$axios.post("/contact/new/form",data,{
+            timeout:5000,
             baseURL:"http://localhost:9000/api"
           })
           //查询一下数据库 将最新的(包括其他系统对数据库的操作)列表渲染到界面上
           //list数组会得到更新
           //一定要先更新list 在更新id 不然会报错 可是这个错也不会影响具体的效果
+          //this.list = 最新的list
           await this.updateList()
         }
 
@@ -108,13 +117,22 @@
       },
 
       // 删除联系人
-      onDelete(info) {
+      async onDelete(info) {
         this.showEdit = false; //隐藏编辑页
-        this.list = this.list.filter((item) => item.id !== info.id);
+
+        //发请求
+        await this.$axios.delete("/contact",{
+          baseURL:"http://localhost:9000/api",
+          params: {
+            id: info.id
+          }
+        })
+
         //如果删除的是选中的那一条  那么选中id要置为nulll
         if (this.chosenContactId === info.id) {
           this.chosenContactId = null;
         }
+        await this.updateList()
       },
 
       // 选中联系人
@@ -139,8 +157,8 @@
       [Popup.name]:Popup
     },
 
-    mounted(){
-      this.updateList()
+    async mounted(){
+      await this.updateList()
     }
   };
 </script>
