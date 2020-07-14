398f73e86b0bce3d08657075b834e35eab22de46
zdylg <zdylg@1314.com>
2020-07-14 11:01:25 +0800

    day12 -2

---
+++ work/day13(mvvm)/MVVM原理/MVVM/mvvm.js
@@ -0,0 +1,39 @@
+function MVVM(options) {
+    // this : vm
+    this.$options = options;
+    var data = this._data = this.$options.data;
+    var me = this;
+
+    //Object.keys(data) : 返回对应对象的可枚举属性的名字!!
+    Object.keys(data).forEach(function(key) {
+        me._proxy(key);
+    });
+
+    observe(data, this);
+
+    this.$compile = new Compile(options.el || document.body, this)
+}
+
+MVVM.prototype = {
+    $watch: function(key, cb, options) {
+        new Watcher(this, key, cb);
+    },
+
+    //key : data中的每一个键名
+    //_proxy方法调用的次数和data中属性的个数有关
+    _proxy: function(key) {
+        var me = this; // me : vm
+
+        //给vm对象添加新的属性!!  属性的名字来至于data配置
+        Object.defineProperty(me, key, {
+            configurable: false,
+            enumerable: true,
+            get: function proxyGetter() {
+                return me._data[key];
+            },
+            set: function proxySetter(newVal) {
+                me._data[key] = newVal;
+            }
+        });
+    }
+};
--- work/day13(mvvm)/js/util.js
+++ work/day13(mvvm)/js/util.js
@@ -1,17 +1,24 @@
 //对Object对象进行深度冻结
 Object.deepFreeze = function (obj) {
-    var propsNames = Object.getOwnPropertyNames(obj)
+    //看门狗
+    if(Object.prototype.toString.call(obj) !== "[object Object]"){
+        throw new Error("第一个参数不是Object对象");
+    }
 
+    var propsNames = Object.getOwnPropertyNames(obj)
     propsNames.forEach(function (key) {
+        //第一次进来的val:"damu"
+        //第二次进来的val:{
+        //             name:"dy"
+        //         }
         var val = obj[key];
         //可以安全的确定val肯定是一个 Object对象
         if(Object.prototype.toString.call(val) === "[object Object]"){
             Object.deepFreeze(val)
-        }else {
-            throw new Error("第一个参数不是一个Object对象")
         }
     })
-    
+
+    //真正做冻结的代码
     return Object.freeze(obj)
 }
 //让我们定义的deepFreeze方法不可枚举
