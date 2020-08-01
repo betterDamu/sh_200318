b2c61adf775a490dfac178fbf3568034796ffd50
zdylg <zdylg@1314.com>
2020-08-01 16:14:55 +0800

    mongoose

--- work/day26(mongoose)/mongoose_exe/app/controllers/user.js
+++ work/day26(mongoose)/mongoose_exe/app/controllers/user.js
@@ -7,8 +7,13 @@ module.exports={
     async addUser(ctx){
         //对前端传来的数据进行检验
         ctx.verifyParams({
+            //不写required 默认required为true
             name:{type:"string",required:true},
-            password:{type:"string",required:true}
+            password:{type:"string",required:true},
+            gender:{type:"string",required:true},//性别
+            avatarUrl:{type:"string",required:false},//头像
+            headLine:{type:"string",required:false},//一句话简介
+            education:{type:"array",itemType:"object",required:false}//教育经历
         })
         //拿到前端传过来的name & password
         let name = ctx.request.body.name
@@ -23,10 +28,11 @@ module.exports={
         //当前这个password是明文 我们进行加密处理
         const salt = bcrypt.genSaltSync(10);
         password = bcrypt.hashSync(password,salt)
+        ctx.request.body.password = password;
 
         //连接数据库 往数据库中新增一个文档
         //create方法返回的是一个promise 这个promise持有的值就是刚刚入库的数据
-        let user = await userModel.create({name,password});
+        let user = await userModel.create(ctx.request.body);
         //query本质上是一个promise 当前promise持有的值就是查询得到的文档
         user = await userModel.findById(user._id)
 
@@ -68,32 +74,45 @@ module.exports={
             path:`http://localhost:8000/img/${name}`
         }
     },
-
-    getAllUser(ctx){
+    //查询所有用户(分页&模糊查询)
+    async getAllUser(ctx){
+        //const page = +ctx.query.page;//显示第几页
+        //const num = +ctx.query.num;//每页显示几条
+        let {page=1,num=10,q=""} = ctx.query;
+        page = Math.max(1,+page);
+        num = Math.max(1,+num);
+        const users = await userModel.find({name:new RegExp(q)}).skip((page-1)*num).limit(num)
         ctx.body=users
     },
-    getUserById(ctx){
+    //根据id查询用户
+    async getUserById(ctx){
         //先决条件
-        const id = +ctx.params.id;
-        if(id < 0){
-            ctx.throw(412,"id不能小于0")
-        }
-        const user = users.find(item => item.id === id)
+        const id = ctx.params.id;
+        const user = await userModel.findById(id)
         ctx.body = user
     },
-    updateUserById(ctx){
-        let name = ctx.request.body.name;
-        let age = ctx.request.body.age;
-        const id = +ctx.params.id;
-        const user = users.find(item => item.id === id);
-        name ? user.name =name:"";
-        age ? user.age =age:"";
+    //根据id修改用户
+    async updateUserById(ctx){
+        ctx.verifyParams({
+            //不写required 默认required为true
+            name:{type:"string",required:false},
+            password:{type:"string",required:false},
+            gender:{type:"string",required:false},//性别
+            avatarUrl:{type:"string",required:false},//头像
+            headLine:{type:"string",required:false},//一句话简介
+            education:{type:"array",itemType:"object",required:false}//教育经历
+        })
+
+        const id = ctx.params.id;
+
+        const user = await userModel.findByIdAndUpdate(id,ctx.request.body,{new:true})
+
         ctx.body = user;
     },
-    delUserById(ctx){
-        const id = +ctx.params.id;
-        const index = users.findIndex(item => item.id === id);
-        users.splice(index,1);
+    //根据id删除用户
+    async delUserById(ctx){
+        const id = ctx.params.id;
+        await userModel.findByIdAndRemove(id)
         ctx.status=204;
     },
 }
